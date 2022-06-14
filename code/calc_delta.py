02ada2999b1ce43796ce4e41978aabcb430fd705
Tiernan8r <tiernan8r@pm.me>
2022-06-14 14:23:58 +0100

    variables renaming in calc_delta.py

--- code/calc_delta.py
+++ code/calc_delta.py
@@ -32,35 +32,37 @@ for sto, snapshot in yt.parallel_objects(snapshots, n_snapshots, storage=storage
 
     units = {"length": (1.0, "Mpc/h")}
 
-    snapshot_f_name = f"/disk12/legacy/GVD_C700_l1600n2048_SLEGAC/dm_gadget/data/snapdir_{snapshot:0>3}/snapshot_{snapshot:0>3}.0.hdf5"
+    f_name = f"/disk12/legacy/GVD_C700_l100n256_SLEGAC/dm_gadget/data/snapdir_{snapshot:0>3}/snapshot_{snapshot:0>3}.0.hdf5"
 
     # load in the simulation
-    data_set = yt.load(snapshot_f_name, unit_base=units)
+    ds = yt.load(f_name, unit_base=units)
 
-    z = data_set.current_redshift
+    z = ds.current_redshift
 
     a = 1 / (1+z)
 
     # define a region containing the full box and get its density
-    region = data_set.r[:, :, :]
+    rg = ds.r[:, :, :]
 
-    rho_bar = region.quantities.total_mass(
-    )[1] / (data_set.domain_width[0]*a)**3
+    rho_bar = rg.quantities.total_mass(
+    )[1] / (ds.domain_width[0]*a)**3
 
     # overdensities
     delta = []
 
     # give units to the sphere radius
-    radius_units = radius * data_set.units.Mpc / data_set.units.h
+    radius_units = radius * ds.units.Mpc / ds.units.h
 
     R = radius_units.to('code_length')
 
     # create spheres at random locations from the file and get their overdensities
     for centres in random_centres:
 
-        sp = data_set.sphere(centres, R)
+        sp = ds.sphere(centres, R)
 
-        rho = sp.quantities.total_mass()[1] / (4*np.pi/3 * (a*R)**3)
+        V = 4/3 * np.pi * (a*R)**3
+
+        rho = sp.quantities.total_mass()[1] / V
 
         delta.append((rho - rho_bar) / rho_bar)
 
