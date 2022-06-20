9a80b19136acd4c8cf1e030784b381f9bdfb2afd
Tiernan8r <tiernan8r@pm.me>
2022-06-20 17:05:36 +0100

    Squashed commit of the following:
    commit e9e2083e1fc165f8f30c25152937f49406e325b7
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 16:08:00 2022 +0100
    
        remove separate sbatch plot_mass_fn*.sh files
    
    commit d6c6651bd2a264337a694ecc6d37e9cb6c42229f
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 16:07:36 2022 +0100
    
        remove separate plot_mass_fn*.py files
    
    commit 3015b5c23e080ce5f616b8ae33af7dd88ae29f56
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 16:06:33 2022 +0100
    
        plots naming reflected in file location
    
    commit 9d33e403922a0ef29d7a3843edfbff04f07316a8
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 16:02:14 2022 +0100
    
        increase timeout on sbatch script for delta.sh
    
    commit 1993925bb725890db33c1b184501091b1f578818
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:55:11 2022 +0100
    
        use os.makedirs to create nested missing directories
    
    commit 223f8d5cc6d47fe32d93c03c20d510fd056e8974
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:36:04 2022 +0100
    
        modification to plots save location
    
    commit 69884fa0e57ad4b03714e06e3c5fa6f940d32443
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:32:13 2022 +0100
    
        add plots/delta/
    
    commit a99c5d99090b60e1d5ccf3b74755089eb21e5e3e
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:27:55 2022 +0100
    
        create sbatch file for delta.sh
    
    commit d06a56454a9de50a6936044db4a146d924d8a06d
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:22:10 2022 +0100
    
        fix to helpers.py halo_finding
    
    commit 185d1a5781a9fd77a3b78f189f2be34fd75563d2
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:08:06 2022 +0100
    
        patch to ensure dirs only created once
    
    commit 0d319b98b1b2367f5952bedb90592044635b1e1e
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:04:13 2022 +0100
    
        untrack delta.npy and z.npy, and add to .gitignore
    
    commit 72aaba73f10ff7f1bd5403de9afc143ecc071bd4
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:03:35 2022 +0100
    
        add log messages and some error handling to delta.py
    
    commit f58b7ed5e4d0dd3c548842b68699d5ce49221cbe
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 15:03:07 2022 +0100
    
        modify mass_fn.py to make the dirs before saving plots to them
    
    commit 79c5993e585f7bda32987dbe4190625de3eb912e
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 14:33:33 2022 +0100
    
        deparallelise code
    
    commit 09b107bacbf603d9bae01f82a54269b5c1705159
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 14:17:27 2022 +0100
    
        organise imports
    
    commit 50f3a6da76e68d50d58e01582ae217d8b7e04db8
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 14:17:04 2022 +0100
    
        reorganise some code & remove cli.py
    
    commit 1607024422990d9049bd994c3b3c571e8ac6a6f7
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 20 14:10:14 2022 +0100
    
        merge calc_delta.py plot_delta.py into delta.py

---
+++ code/delta.py
@@ -0,0 +1,147 @@
+import os
+from typing import Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import yt
+from unyt.exceptions import IterableUnitCoercionError
+
+import helpers
+
+# MPI Stuff
+NUM_SNAPSHOTS = 102
+# Sphere radius
+RADIUS = 50
+
+ROOT = "/disk12/legacy/"
+SIMULATION = "GVD_C700_l600n2048_SLEGAC"
+SIMULATION_PATH = ROOT + SIMULATION + "/"
+
+PLOTS_PATH = "../plots/delta/{0}/"
+PLOTS_FILENAME = "delta_vs_a_final_50Mpc.png"
+
+
+def main():
+    z_arr, delta_arr = calc()
+    save(z_arr, delta_arr)
+    plot(z_arr, delta_arr)
+
+
+def calc() -> Tuple[np.ndarray, np.ndarray]:
+    # Sphere centers from a file
+    random_centres = np.loadtxt("examples/random_locs.txt", max_rows=500)
+    print("loaded", len(random_centres), "random coordinates from the file")
+
+    _, _, snapshots = helpers.find_halos(SIMULATION_PATH)
+
+    storage = {}
+
+    print("Beginning iteration over", len(snapshots), "snapshots")
+
+    for snap_f_name in snapshots:
+        print("Working on snapshot:", snap_f_name)
+
+        units = {"length": (1.0, "Mpc/h")}
+
+        # load in the simulation
+        ds = yt.load(snap_f_name, unit_base=units)
+
+        z = ds.current_redshift
+        print("Redshift is:", z)
+
+        a = 1 / (1+z)
+
+        # define a region containing the full box and get its density
+        rg = ds.r[:]
+
+        try:
+            rho_bar = rg.quantities.total_mass(
+            )[1] / (ds.domain_width[0]*a)**3
+        except IterableUnitCoercionError as e:
+            print("Error reading regions quantities from database, ignoring...")
+            print(e)
+            continue
+
+        # overdensities
+        delta = []
+
+        # give units to the sphere radius
+        radius_units = RADIUS * ds.units.Mpc / ds.units.h
+
+        R = radius_units.to('code_length')
+
+        # create spheres at random locations from the file and get their overdensities
+        for i in range(len(random_centres)):
+            centres = random_centres[i]
+
+            sp = ds.sphere(centres, R)
+
+            V = 4/3 * np.pi * (a*R)**3
+
+            try:
+                rho = sp.quantities.total_mass()[1] / V
+            except IterableUnitCoercionError as e:
+                print(
+                    i, "- Error reading regions quantities from database, defaulting to rho_bar...")
+                print(e)
+                rho = rho_bar
+
+            delta.append((rho - rho_bar) / rho_bar)
+
+        print("Finished calculating delta for snapshot...")
+        print("-----\n")
+        # store results from each process
+        storage[z] = delta
+
+    return wrangle(storage)
+
+
+def wrangle(storage: dict) -> Tuple[np.ndarray, np.ndarray]:
+    deltas = []
+    z = []
+
+    # get the data from storage
+    for zs, dels in sorted(storage.items()):
+        z.append(zs)
+        deltas.append(dels)
+
+    z_arr = np.array(z)
+    delta_arr = np.array(deltas)
+
+    return z_arr, delta_arr
+
+
+def save(z_arr: np.array, delta_arr: np.array):
+    print("Saving data to file...")
+    # write the data
+    with open("../data/z.npy", "wb") as f:
+        np.save(f, z_arr)
+
+    with open("../data/delta.npy", "wb") as f:
+        np.save(f, delta_arr)
+
+
+def plot(z_arr: np.ndarray, delta_arr: np.ndarray):
+    # Sphere centers from a file
+    c = np.loadtxt("examples/random_locs.txt", max_rows=500)
+
+    a_arr = 1 / (1+z_arr)
+
+    # plot the data
+    for i in range(len(c)):
+        plt.plot(a_arr, delta_arr.T[i], alpha=0.1)
+
+    plt.title("Overdensity evolution with cosmic scale factor")
+    plt.xlabel("Scale factor $a$")
+    plt.ylabel("Overdensity $\delta$")
+
+    dir_name = PLOTS_PATH.format(SIMULATION)
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+    
+    plt_f_name = PLOTS_PATH + PLOTS_FILENAME
+    plt.savefig(plt_f_name.format(SIMULATION))
+
+
+if __name__ == "__main__":
+    main()
--- code/sbatch/delta.sh
+++ code/sbatch/delta.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 #SBATCH -p all
-#SBATCH --job-name=plot_delta
-#SBATCH -o logs/out_plot_delta.log
+#SBATCH --job-name=delta
+#SBATCH -o logs/delta.log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=2:00:00
-#SBATCH --mem=1G
-
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+#SBATCH --mem=64G
 # email notifications (NONE, BEGIN, END, FAIL, REQUEUE, ALL)
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s2222340@ed.ac.uk
 
 #################
 
+NTASKS=1
+NTHREADS=1
+
 cd ${SLURM_SUBMIT_DIR}
-
-# Load your anaconda module if needed
-
 . activate_environment.sh
 
-python plot_delta.py
+srun -n ${NTASKS} -c ${NTHREADS} python delta.py
