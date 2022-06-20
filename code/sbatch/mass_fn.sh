6d1164eb215054091ec1143567c4141ef0cf9198
Tiernan8r <tiernan8r@pm.me>
2022-06-20 12:23:39 +0100

    rename simple_mass_fn.sh -> mass_fn.sh and patch paths

--- code/sbatch/mass_fn.sh
+++ code/sbatch/mass_fn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH -p all
-#SBATCH --job-name=simple_mass_fn
-#SBATCH -o logs/simple_mass_fn.log
+#SBATCH --job-name=mass_fn
+#SBATCH -o logs/mass_fn.log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
@@ -21,4 +21,4 @@ cd ${SLURM_SUBMIT_DIR}
 
 . activate_environment.sh
 
-srun -n ${ntasks} -c ${nthreads} python simple_mass_fn.py
+srun -n ${ntasks} -c ${nthreads} python mass_fn.py
