ee527b40d93e8b29dd27167eeb1bab242841fd6e
Tiernan8r <tiernan8r@pm.me>
2022-06-20 12:19:45 +0100

    increase the timeout

--- code/sbatch/simple_mass_fn.sh
+++ code/sbatch/simple_mass_fn.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=12:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=64G
 # email notifications (NONE, BEGIN, END, FAIL, REQUEUE, ALL)
 #SBATCH --mail-type=ALL
