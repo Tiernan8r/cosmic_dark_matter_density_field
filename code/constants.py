390b86c0a3b3db99f7740692c8f2d58b75a993b7
Tiernan8r <tiernan8r@pm.me>
2022-06-23 16:51:18 +0100

    Squashed commit of the following:
    commit ca0608bd8c4bd333bd5137be4f20462ff629e841
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 16:51:02 2022 +0100
    
        remove nesting of logs output to logs/
    
    commit ed4610bb6f787d9fe666a1b1486d2cbf9fadde30
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 16:49:03 2022 +0100
    
        Only run `main()` on root node:
    
        Change code to run `main()` steps within `yt.is_root()` check.
    
        Stop parallel running over multiple rockstar files, but keep
        parallelisation over coords.
    
        Ensure halo mass units are Msun/h
    
        Modifications to sbatch parameters when running in parallel.
    
    commit 28b60a4df920bec5fae622e26a1f1ea28abd5eef
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 15:52:31 2022 +0100
    
        make some values that were calculated using regex matching now be calced
        using the constants or from the dataset.
        This allows the method signature of total_mass_function() to be
        simplified.
    
        Only run main() on the main thread.
    
    commit d6faae19328f8d30d47358058997ea827d77250b
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 14:53:40 2022 +0100
    
        reduce timeout to 24hr
    
    commit b2686260720096740c87b69093ecd82031966188
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 14:47:23 2022 +0100
    
        change to using .json over .yaml for log config
    
    commit 5298bc86b72608adb55c63b4f4155c11f934d01c
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 14:32:52 2022 +0100
    
        remove re as import
    
    commit a87c8b54638fd971153411a562b87b360a623ecf
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 14:32:32 2022 +0100
    
        change error catch type in code
    
    commit db2845b1668aae93e32706637b2e7dde41f2ae12
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 14:29:39 2022 +0100
    
        finish running plots
    
    commit ac73f6302733f46b6fb68cb7e3a2af197a600ef0
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 12:57:01 2022 +0100
    
        Updates to `helpers.py`:
    
        Move cache keys to constants.py. Add logging messages to all function
        calls.
    
    commit 37d6ddd3bd68bae4be28f052aa74d86eb76b46ae
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 12:36:29 2022 +0100
    
        move constants from helpers.py to constants.py

--- code/constants.py
+++ code/constants.py
@@ -2,15 +2,13 @@ import re
 
 # Log file parameters
 
-LOG_FILENAME = "logging.yaml"
+LOG_FILENAME = "logging.json"
 
 # Data set parameters
 
 ROOT = "/disk12/legacy/"
 SIM_NAME = "GVD_C700_l1600n2048_SLEGAC"
 SIM_FOLDER = f"{SIM_NAME}/"
-
-sim_regex = re.compile("^.*(GVD_C(\d{3})_l(\d+)n(\d+)_SLEGAC).*$")
 
 # Sampling parameters
 
@@ -31,3 +29,65 @@ MASS_FN_PLOTS_FNAME_PTRN = MASS_FN_PLOTS_DIR + \
 OVERDENSITY_PLOTS_DIR = "../plots/deltas/{0}/"
 OVERDENSITY_PLOTS_FNAME_PTRN = OVERDENSITY_PLOTS_DIR + \
     "overdensity_r{1:.2f}-z{2:.2f}.png"
+
+# Constants for helpers.py:
+
+# All data set names
+DATA_SETS = [
+    "GVD_C700_l100n1024_SLEGAC/",
+    "GVD_C700_l100n2048_SLEGAC/",
+    "GVD_C700_l10n1024_SLEGAC/",
+    "GVD_C700_l1600n2048_SLEGAC/",
+    "GVD_C900_l100n2048_SLEGAC/",
+    "GVD_C900_l1600n2048_SLEGAC/",
+]
+TEST_DATA_SETS = [
+    "GVD_C700_l100n256_SLEGAC/",
+    "GVD_C700_l1600n256_SLEGAC/",
+    "GVD_C700_l1600n64_SLEGAC/",
+]
+ALL_DATA_SETS = sorted(DATA_SETS + TEST_DATA_SETS)
+
+# List of paths to all the data sets
+
+PATHS = [ROOT + ds for ds in DATA_SETS]
+TEST_PATHS = [ROOT + tds for tds in TEST_DATA_SETS]
+ALL_PATHS = [ROOT + ads for ads in ALL_DATA_SETS]
+
+# Subpath for rockstar files
+ROCKSTAR = "dm_gadget/rockstar/"
+# Subpath for snapshot/group files
+DATA = "dm_gadget/data/"
+
+# Naming pattern for group files
+GROUP_PATTERN = DATA + "groups_{0:0>3}/fof_subhalo_tab_{0:0>3}.0.hdf5"
+# Naming pattern for snapshot files
+SNAPSHOT_PATTERN = DATA + "snapdir_{0:0>3}/snapshot_{0:0>3}.0.hdf5"
+# Naming pattern for rockstar files
+ROCKSTAR_PATTERN = ROCKSTAR + "halos_{0:0>3}.0.bin"
+
+# Regexes to match against the appropriate file naming convention
+
+groups_regex = re.compile("^fof_(subhalo_)?tab_\d{3}.0.hdf5$")
+rockstar_regex = re.compile("^halos_\d{3}.0.bin$")
+snapshots_regex = re.compile("^snapshot_\d{3}.0.hdf5$")
+
+# Regex to match against the root dir rockstar files are stored in
+rockstar_root_regex = re.compile(".*rockstar/$")
+
+# Regex to match against the naming pattern of the simulation data sets
+sim_regex = re.compile("^(GVD_C(\d{3})_l(\d+)n(\d+)_SLEGAC)$")
+
+# Regex to find redshifts from rockstar files
+rockstar_ascii_reg = re.compile("^(halos_(\d+).0).ascii$")
+rockstar_a_factor = re.compile("^#a = (.*)$")
+
+# Path to the cache used to optimise helpers functions
+PATH_TO_CACHE = "../data/helpers.pickle"
+
+# Keys used in the helpers cache:
+REDSHIFTS_KEY = "redshifts"
+DIR_KEY = "dirs"
+SNAPSHOTS_KEY = "snapshots"
+GROUPS_KEY = "groups"
+ROCKSTARS_KEY = "rockstars"
