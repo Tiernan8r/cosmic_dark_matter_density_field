dcb7515f9db91c4796bb42dc18004682b104992a
Tiernan8r <tiernan8r@pm.me>
2022-06-23 12:29:34 +0100

    Squashed commit of the following:
    commit 411b9306d436d4c3e87537a30c7df2817d2d4bec
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 12:29:12 2022 +0100
    
        run sbatch using mpirun
    
    commit dc5b22a3e395906471f09248ccbdff145f71bbbd
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Thu Jun 23 12:18:02 2022 +0100
    
        initial parallelisation attempt

--- code/calculations.sh
+++ code/calculations.sh
@@ -5,20 +5,21 @@
 #SBATCH -o logs/calculations.log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --time=72:00:00
-#SBATCH --mem=1G
+#SBATCH --mem=8G
 # email notifications (NONE, BEGIN, END, FAIL, REQUEUE, ALL)
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s2222340@ed.ac.uk
 
 #################
 
-ntasks=1
+ntasks=8
 nthreads=1
 
 cd ${SLURM_SUBMIT_DIR}
 
 . activate_environment.sh
 
-srun -n ${ntasks} -c ${nthreads} python calculations.py
+# srun -n ${ntasks} -c ${nthreads} python calculations.py
+mpirun -np ${ntasks} python calculations.py
