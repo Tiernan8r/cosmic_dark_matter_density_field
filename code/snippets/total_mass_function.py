f862e4c2935bfadacf7d7d2edc5d82eaba60fda2
Tiernan8r <tiernan8r@pm.me>
2022-06-28 13:28:03 +0100

    Squashed commit of the following:
    commit 128d95e10d239282a208b9786c9dc9f75d3fd8f1
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:50:51 2022 +0100
    
        snippets for each type
    
    commit 8db4de360fce36eefd8188b698279feb503168c8
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:17:31 2022 +0100
    
        reduce the number of histogram bins to use by default
    
    commit 3fd0b1d20fc85ec3e2e0cfc635d8cc5e4cbd9574
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:17:12 2022 +0100
    
        use the massive configuration by default in calculations.sh
    
    commit acee16d1fcbebb603a57bd249a4a5346f1473732
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:16:41 2022 +0100
    
        fixes to how fig/ax labels & title set in plotting.py
    
    commit 122757f426e4344f07284927c90f26564413a9e9
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:16:20 2022 +0100
    
        more logging in plotting.py
    
    commit 465dd84e40af4a40cf89589780b31ea1345beb6c
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:15:57 2022 +0100
    
        remove tweaking mass units in total mass fn
    
    commit c62df25327177fe95ef22f1ac61509991f7079d0
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:15:36 2022 +0100
    
        tweak to plot title for total mass fn
    
    commit e609566a787bfb85f1872f880aa497ae2ec28d2f
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:15:18 2022 +0100
    
        use proper distance in calculations
    
    commit c99317c723a62c5649b994e53da0558662712eb8
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:14:47 2022 +0100
    
        set number of hist bins to use in total mass function
    
    commit 0a0785fc5a96afd1cebba332bb7a925da9146a29
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:14:24 2022 +0100
    
        add config actions to ignore the cached values and force recalculation
    
    commit 75843fc541df36370121392a2890f7b5e6a803ae
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:12:59 2022 +0100
    
        add simple config to run only once instance
    
    commit 57bee4837ce2c2bbfea77fc4a7843223efae5249
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Tue Jun 28 12:12:36 2022 +0100
    
        another plots run
    
    commit d281ad668e33e61c31a47d2623c8fc2dc4e7219d
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 27 16:23:55 2022 +0100
    
        another partial run
    
    commit 5f604d37444bb7f863bde1b131024124672e611f
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 27 15:52:47 2022 +0100
    
        add a config file for big calculation
    
    commit 338b206ba874d0ee97fb8331f6d22d8ff71bfe17
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 27 15:50:21 2022 +0100
    
        fix on dict accessing and typos
    
    commit 761cb7d409783a7d2da3bc21c8f84f0cbea94dcb
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jun 27 15:49:47 2022 +0100
    
        plot on unique fig/axes

---
+++ code/snippets/total_mass_function.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import yt
+import yt.extensions.legacy
+
+# halo file 101 corresponds to z=0
+REDSHIFT_NUMBER = 101
+DATAFILE = "/disk12/legacy/GVD_C700_l1600n2048_SLEGAC/dm_gadget/rockstar/halos_{0:>3}.0.bin"
+
+# Number of histogram bins to use
+NUM_BINS = 100
+
+fname = DATAFILE.format(REDSHIFT_NUMBER)
+
+ds = yt.load(fname)
+ad = ds.all_data()
+
+masses = ad["halos", "particle_mass"].to(ds.units.Msun)
+
+print("Mass units:", masses.units)
+
+# Volume of the box
+sim_size = ds.domain_width[0].to(ds.units.Mpc)
+volume = sim_size ** 3
+
+print("Volume of simulation:", volume)
+
+# Calculate the histogram of the masses
+hist, bins = np.histogram(masses, bins=NUM_BINS)
+
+# Filter hist/bins for non-zero masses
+valid_idxs = np.where(hist > 0)
+hist = hist[valid_idxs]
+bins = bins[valid_idxs]
+
+# convert histogram values to number density
+hist = hist / volume
+
+z = ds.current_redshift
+
+x = np.log(bins)
+y = np.log(hist)
+
+fig = plt.figure()
+ax = fig.gca()
+
+ax.plot(x, y)
+ax.set_xscale("log")
+
+title = f"Total Mass Function for z={z:.2f}"
+fig.suptitle(title)
+
+ax.set_xlabel("$\log{M_{vir}}$")
+ax.set_ylabel("$\phi=\\frac{d \log{n}}{d \log{M_{vir}}}$")
+
+plot_fname = f"total_mass_function_z{z}.png"
+fig.savefig(plot_fname)
