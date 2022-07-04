4214ec9dbcf135473bbb7ea5537ab1f5b4fb2810
Tiernan8r <tiernan8r@pm.me>
2022-07-04 11:48:33 +0100

    Squashed commit of the following:
    commit c27d6b630987b5983f4ef86d7208cbbec6b71537
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Mon Jul 4 11:48:14 2022 +0100
    
        plots from run so far
    
    commit 7ee37cf475ffdade977fb8f1e4a6503480b827bd
    Author: Tiernan8r <tiernan8r@pm.me>
    Date:   Fri Jul 1 16:51:19 2022 +0100
    
        snippet to highlight code issue

---
+++ code/snippets/integer_required.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import yt
+import yt.extensions.legacy
+
+# halo file 101 corresponds to z=0
+REDSHIFT_NUMBER = 22
+DATAFILE = "/disk12/legacy/GVD_C700_l1600n2048_SLEGAC/dm_gadget/rockstar/halos_{0:0>3}.0.bin"
+
+fname = DATAFILE.format(REDSHIFT_NUMBER)
+
+ds = yt.load(fname)
+ad = ds.all_data()
